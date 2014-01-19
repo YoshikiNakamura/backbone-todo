361822ef93b6fdfb019c4c3d4913cde81fcf1048
Yoshiki <yn.phy6@gmail.com>
2014-01-19 13:53:40 +0900

    第３回：モデル作成、即時関数のデバッグ

--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
 (function(){
 
-});
+	//モデル
+	var Task = Backbone.Model.extend({
+		defaults: {
+			title: "do something",
+			completed: false
+		}
+	});
+
+	var task1 = new Task({
+		completed: true
+	});
+
+	console.log(task1.toJSON());
+})();
